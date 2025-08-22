afea689dd3e1893011869581d5b23310dd97f988
Infinity <wemersoncontas22@gmail.com>
2025-08-22 16:48:37 -0300

    fix(home): correct profile image path to use public/images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,10 @@ export default function Home() {
       <div className="flex-1 text-center md:text-left">
         <h1 className="text-4xl md:text-6xl font-bold">
           Hi There! <br />
-          I&apos;m <span className="text-yellow-500">Benjamin</span>
+          I&apos;m <span className="text-purple-500">Wemerson</span>
         </h1>
-        <p className="mt-4 text-gray-600 text-lg">
-          Graphic Designer & Photographer
-        </p>
-        <button className="mt-6 px-6 py-3 bg-yellow-500 text-black font-semibold rounded-xl hover:bg-yellow-400 transition">
+        <p className="mt-4 text-gray-600 text-lg">Backend Developer</p>
+        <button className="mt-6 px-6 py-3 bg-purple-500 text-black font-semibold rounded-xl hover:bg-purple-400 transition">
           More About Me
         </button>
       </div>
@@ -27,7 +25,7 @@ export default function Home() {
         className="flex-1 flex justify-center mt-10 md:mt-0"
       >
         <img
-          src="../public/io"
+          src="/images/ProfilePhoto.JPG"
           alt="Profile Photo"
           className="rounded-2xl shadow-lg w-72 md:w-96"
         />
