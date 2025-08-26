970705c24f3f77b83dd8b0b878c262c1d57a6f83
Infinity <wemersoncontas22@gmail.com>
2025-08-26 14:58:41 -0300

    fix(projects): update repository links in portfolio

--- src/lib/constants/project.ts
+++ src/lib/constants/project.ts
@@ -12,7 +12,7 @@ export const projects: Project[] = [
     title: "BeWear",
     description:
       "A modern web e-commerce app built with Next.js, React, Tailwind CSS, and PostgreSQL, featuring a shopping cart, Stripe checkout, and user authentication.",
-    link: "https://github.com/wmsalves",
+    link: "https://github.com/wmsalves/bewear",
     tags: ["Next.js", "React", "Tailwind", "PostgreSQL", "Stripe"],
     image: "/images/BeWear.png",
     slug: "bewear",
@@ -21,7 +21,7 @@ export const projects: Project[] = [
     title: "Salgadin",
     description:
       "A web application to manage personal finances with daily spending summaries and visual charts highlighting where your money goes.",
-    link: "https://github.com/wmsalves",
+    link: "https://github.com/wmsalves/Salgadin",
     tags: ["C#", ".ASP NET", "React", "Tailwind", "SQL Server", "Azure"],
     image: "/images/Salgadin.png",
     slug: "salgadin",
@@ -30,7 +30,7 @@ export const projects: Project[] = [
     title: "Blog",
     description:
       " A simple blog application demonstrating CRUD (Create, Read, Update, Delete) operations using Node.js, Express, EJS, and MongoDB. It provides a basic setup for building a full-stack application with a MongoDB database, user authentication, and dynamic content rendering.",
-    link: "https://github.com/wmsalves",
+    link: "https://github.com/wmsalves/blog-node-mongo",
     tags: ["NodeJS", "Express", "EJS", "MongoDB"],
     image: "/images/Sandman.jpg",
     slug: "blog",
