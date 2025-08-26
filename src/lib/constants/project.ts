dfaa251daa678855efaf8d7d06d646854620ab44
Infinity <wemersoncontas22@gmail.com>
2025-08-26 09:33:09 -0300

    feat(projects): add Salgadin image using /public URL (/images/Salgadin.png)

--- src/lib/constants/project.ts
+++ src/lib/constants/project.ts
@@ -11,10 +11,19 @@ export const projects: Project[] = [
   {
     title: "BeWear",
     description:
-      "A modern web e-commerce app built with Next.js, React, Tailwind CSS, and PostgreSQL, featuring a shopping cart, Stripe checkout, and user authentication. ",
+      "A modern web e-commerce app built with Next.js, React, Tailwind CSS, and PostgreSQL, featuring a shopping cart, Stripe checkout, and user authentication.",
     link: "https://github.com/wmsalves",
     tags: ["Next.js", "React", "Tailwind", "PostgreSQL", "Stripe"],
     image: "/images/BeWear.png",
     slug: "bewear",
+  },
+  {
+    title: "Salgadin",
+    description:
+      "A web application to manage personal finances with daily spending summaries and visual charts highlighting where your money goes.",
+    link: "https://github.com/wmsalves",
+    tags: ["C#", ".ASP NET", "React", "Tailwind", "SQL Server", "Azure"],
+    image: "/images/Salgadin.png",
+    slug: "salgadin",
   },
 ];
